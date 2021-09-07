55a99ade66a367c5dd800ffb5f0827d26531e7b4
Kevin Hernandez <kev.hernandezgarza@gmail.com>
2021-09-07 18:41:37 -0500

    added a fetch request

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,6 +1,17 @@
 // creating function call
-var getUserRepos = function () {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function (user) {
+    // format the github api url
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    // make a request to the url 
+    fetch(apiUrl).then(function (response) {
+        response.json().then(function (data) {
+            console.log(data);
+        });
+    });
 };
 
 getUserRepos();
+
+var response = fetch("https://api.github.com/users/octocat/repos");
+console.log(response);
